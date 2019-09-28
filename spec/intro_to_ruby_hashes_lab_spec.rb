5ddca04f5f6c416df789ba9d4b0381d70caa561d
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-28 16:14:36 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -1,2 +1,18 @@
 new_hash = Hash.new
 
+my_hash = {
+  :name => "Kisha"
+}
+
+poineer = {
+  :name = 'Grace Hopper'
+}
+
+id_generator = {
+  :id = 'King'
+}
+my_hash_creator = {
+  :name = "Todd"
+  :random = "Fish"
+}
+
